2e50cd314dd11e7d37fe2361be9dc6f8d595f6d6
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-07 11:34:46 -0300

    feat: add estilizando elementos

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
-import { SafeAreaView, Text } from "react-native";
+import React from "react";
+import { SafeAreaView, Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Olá Mundo</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.mainEvent}>Nome do evento</Text>
+        <Text style={styles.localEvent}>Sexta, 4 de Novembro de 2022.</Text>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#131016",
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  mainEvent: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  localEvent: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+});
