62b37be44d61bbea84cba5d035c914829319d935
thiagoperest <thiago_teodoroperes07@hotmail.com>
2023-10-07 16:00:41 -0300

    feat: add botão de adicionar participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.mainEvent}>Nome do evento</Text>
@@ -12,6 +16,10 @@ export function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -22,4 +22,16 @@ export const styles = StyleSheet.create({
     padding: 16,
     fontSize: 16,
   },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 24,
+  },
 });
